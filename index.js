dcc43f9594ce1e49f1b4e5d28625b621fa98bb9d
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-10 16:36:13 -0500

    added questions

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -12,3 +11,60 @@ function init() {}
 
 // Function call to initialize app
 init();
+ //
+
+ const inquirer = require('inquirer');
+ const fs = require('fs');
+
+ inquirer.prompt(
+     {
+         type: input,
+         name: 'title',
+         message: "What is your project name?"
+     },
+     {
+         type: 'input', 
+         name: 'repo',
+         message: 'What is your GitHub repo URL?'
+     },
+     {
+         type: 'input',
+         name: 'description',
+         message: 'How would you describe your project?'
+     },
+     {
+         type: 'confirm',
+         name: 'table confirm',
+         message: 'Do you want a Table of Contents?'
+     },
+     {
+         type: 'input',
+         name: 'installation',
+         message: 'How is your app installed?'
+     },
+     {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage?'
+     },
+     {
+        type: 'input',
+        name: 'license',
+        message: 'What is the license for this project?'
+     },
+     {
+        type: 'input',
+        name: 'contribtors',
+        message: 'Who are the contibutors to this project?'
+     },
+     {
+        type: 'input',
+        name: 'testing',
+        message: 'Please share testing results'
+     },
+     {
+        type: 'input',
+        name: 'questions',
+        message: 'How do people contact you for questions?'
+     },
+ )
