ae5c520079bc90a018970783a897f463c62d87e4
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-10 17:05:04 -0500

    wip js

--- index.js
+++ index.js
@@ -1,68 +1,71 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: input,
-        name: 'title',
-        message: "What is your project name?"
-    },
-    {
-        type: 'input', 
-        name: 'repo',
-        message: 'What is your GitHub repo URL?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'How would you describe your project?'
-    },
-    {
-        type: 'confirm',
-        name: 'table confirm',
-        message: 'Do you want a Table of Contents?'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How is your app installed?'
-    },
-    {
-       type: 'input',
-       name: 'usage',
-       message: 'What is the usage?'
-    },
-    {
-       type: 'input',
-       name: 'license',
-       message: 'What is the license for this project?'
-    },
-    {
-       type: 'input',
-       name: 'contributors',
-       message: 'Who are the contibutors to this project?'
-    },
-    {
-       type: 'input',
-       name: 'testing',
-       message: 'Please share testing results'
-    },
-    {
-       type: 'input',
-       name: 'questions',
-       message: 'How do people contact you for questions?'
-    },
-];
+// const questions = [
+//     {
+//         type: input,
+//         name: 'title',
+//         message: "What is your project name?"
+//     },
+//     {
+//         type: 'input', 
+//         name: 'repo',
+//         message: 'What is your GitHub repo URL?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'description',
+//         message: 'How would you describe your project?'
+//     },
+//     {
+//         type: 'confirm',
+//         name: 'table confirm',
+//         message: 'Do you want a Table of Contents?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'installation',
+//         message: 'How is your app installed?'
+//     },
+//     {
+//        type: 'input',
+//        name: 'usage',
+//        message: 'What is the usage?'
+//     },
+//     {
+//        type: 'list',
+//        name: 'license',
+//        message: 'What is the license for this project?',
+//        choices: ['MIT','GPL','Apache','GNU', 'None']
+//     },
+//     {
+//        type: 'input',
+//        name: 'contributors',
+//        message: 'Who are the contibutors to this project?'
+//     },
+//     {
+//        type: 'input',
+//        name: 'testing',
+//        message: 'Please share testing results'
+//     },
+//     {
+//        type: 'input',
+//        name: 'questions',
+//        message: 'How do people contact you for questions?'
+//     },
+// ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
- //
+// // Function call to initialize app
+// init();
+ 
+
+///////////
 
  const inquirer = require('inquirer');
  const fs = require('fs');
@@ -70,7 +73,7 @@ init();
  inquirer
   .prompt([
      {
-         type: input,
+         type: 'input',
          name: 'title',
          message: "What is your project name?"
      },
@@ -86,7 +89,7 @@ init();
      },
      {
          type: 'confirm',
-         name: 'table confirm',
+         name: 'table',
          message: 'Do you want a Table of Contents?'
      },
      {
@@ -100,9 +103,10 @@ init();
         message: 'What is the usage?'
      },
      {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'What is the license for this project?'
+        message: 'What is the license for this project?',
+        choices: ['MIT','GPL','Apache','GNU', 'None']
      },
      {
         type: 'input',
@@ -120,6 +124,55 @@ init();
         message: 'How do people contact you for questions?'
      },
     ])
+.then(({
+    title, 
+    repo,
+    description,
+    table,
+    installation,
+    usage,
+    license,
+    contributors,
+    testing,
+    questions
+}) => {
+    const readMeTemplatee = 
+    `# ${title}
+    
+    - [Description](#description)
+    - [Table of Contents](#tableofcontents)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributors](#contributors)
+    - [Testing](#testing)
+    - [Questions](#questions)
+
+    # Description
+    ${description}
+    # Table of Contents
+    ${table}
+    # Installation
+    ${installation}
+    ## Usage
+    ${usage}
+    ## License
+    ${license}
+    # Contributors
+    ${contributors}
+    # Testing
+    ${testing}
+    # Questions
+    ${questions}
+
+    Contact Information:
+    - GitHub: ${repo}
+    - Email: ${email}`
+
+    createNewReadme(title, readMeTemplatee);
+})
+
+
 .then((response) => {
      const readMeContent = createReadMe(response);
 
